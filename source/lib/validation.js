b161c2aaf7d4ab90640d682d4ae1020e98aedd63
gilesni <jklihu@gmail.com>
2018-05-25 14:08:30 -0700

    Adding validation.js and the lib folder

---
+++ source/lib/validation.js
@@ -0,0 +1,15 @@
+module.exports = {
+  validateAgainstSchema(obj, schema) {
+    return obj && Object.keys(schema).every(field => !schema[field].required || obj[field]);
+  }
+
+  extractValidFields(obj, schema) {
+    let validObj = {};
+    Object.keys(schema).forEach((field) => {
+      if (obj[field]) {
+        validObj[field] = obj[field];
+      }
+    });
+    return validObj;
+  }
+};
