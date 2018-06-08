d4b3c16119004361039c7b3da562e04021b71845
srb76 <bonnersa@oregonstate.edu>
2018-06-08 01:31:14 -0700

    Fix validation.js

--- source/lib/validation.js
+++ source/lib/validation.js
@@ -1,9 +1,24 @@
 module.exports = {
-  validateAgainstSchema(obj, schema) {
-    return obj && Object.keys(schema).every(field => !schema[field].required || obj[field]);
-  }
+  /*
+   * Performs data validation on an object by verifying that it contains
+   * all required fields specified in a given schema.
+   *
+   * Returns true if the object is valid agianst the schema and false otherwise.
+   */
+  validateAgainstSchema: function (obj, schema) {
+    return obj && Object.keys(schema).every(
+      field => !schema[field].required || obj[field]
+    );
+  },
 
-  extractValidFields(obj, schema) {
+  /*
+   * Extracts all fields from an object that are valid according to a specified
+   * schema.  Extracted fields can be either required or optional.
+   *
+   * Returns a new object containing all valid fields extracted from the
+   * original object.
+   */
+  extractValidFields: function (obj, schema) {
     let validObj = {};
     Object.keys(schema).forEach((field) => {
       if (obj[field]) {
