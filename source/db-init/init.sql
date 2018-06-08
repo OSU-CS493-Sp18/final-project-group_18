9130dba6abcfb032dc3e96ffbf464fe099bac18e
srb76 <bonnersa@oregonstate.edu>
2018-06-08 00:11:53 -0700

    Add initial build of spell route
    -still needs authentication

--- source/db-init/init.sql
+++ source/db-init/init.sql
@@ -1,13 +1,30 @@
 SET Foreign_Key_checks=0;
 
-DROP TABLE IF EXISTS test;
+CREATE DATABASE IF NOT EXISTS gamedb;
 
-CREATE TABLE test(
-  ID int NOT NULL AUTO_INCREMENT,
-  message text,
-  PRIMARY KEY(ID)
-)ENGINE = InnoDB;
+CREATE TABLE player (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255),
+  username varchar(32) NOT NULL,
+  password varchar(32) NOT NULL,
+  email varchar(255) NOT NULL,
+  PRIMARY KEY(ID),
+  UNIQUE(username),
+  UNIQUE(email)
+);
 
-INSERT INTO test VALUES (NULL, "Test Succeeded");
+CREATE TABLE spell (
+	id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	ownerID MEDIUMINT,
+	name VARCHAR(255) NOT NULL,
+	cost MEDIUMINT NOT NULL,
+	damage MEDIUMINT,
+	school VARCHAR(255),
+	PRIMARY KEY (id),
+	FOREIGN KEY (ownerID) REFERENCES player (id)
+);
+
+INSERT INTO player VALUES (NULL, "Bob", "bob", "encrypted", "email@email.com");
+INSERT INTO spell VALUES (NULL, 1, "Fireball", 5, 10, "Fire");
 
 SET Foreign_Key_checks=1;
