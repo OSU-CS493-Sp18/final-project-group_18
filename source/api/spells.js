85489102cb9bde7fc92d28a6c1f8c766a7c70ec7
srb76 <bonnersa@oregonstate.edu>
2018-06-10 14:10:17 -0700

    Changed table name spell to spells

--- source/api/spells.js
+++ source/api/spells.js
@@ -31,7 +31,7 @@ const spellSchema = {
 function getSpellsCount() {
   return new Promise((resolve, reject) => {
     mysqlPool.query(
-      'SELECT COUNT(*) AS count FROM spell',
+      'SELECT COUNT(*) AS count FROM spells',
       function (err, results) {
         if (err) {
           reject(err);
@@ -51,14 +51,14 @@ function getSpellsPage(page, count) {
     page = page > lastPage ? lastPage : page;
     const offset = (page - 1) * numPerPage;
     mysqlPool.query(
-      'SELECT * FROM spell ORDER BY id LIMIT ?,?',
+      'SELECT * FROM spells ORDER BY id LIMIT ?,?',
       [ offset, numPerPage ],
       function (err, results) {
         if (err) {
           reject(err);
         } else {
           resolve({
-            spell: results,
+            spells: results,
             pageNumber: page,
             totalPages: lastPage,
             pageSize: numPerPage,
@@ -92,7 +92,7 @@ router.get('/', function (req, res) {
     .catch((err) => {
 	  console.error(err);
       res.status(500).json({
-        error: "Error fetching spell list."
+        error: "Error fetching spells list."
       });
     });
 });
@@ -100,7 +100,7 @@ router.get('/', function (req, res) {
 function getSpellByID(spellID) {
   return new Promise((resolve, reject) => {
     mysqlPool.query(
-      'SELECT * FROM spell WHERE id = ?',
+      'SELECT * FROM spells WHERE id = ?',
       [ spellID ],
       function (err, results) {
         if (err) {
@@ -117,32 +117,32 @@ router.get('/:spellID', function (req, res, next) {
   const spellID = parseInt(req.params.spellID);
   
   getSpellByID(spellID)
-    .then((spell) => {
-      if (spell) {
-        res.status(200).json(spell);
+    .then((spells) => {
+      if (spells) {
+        res.status(200).json(spells);
       } else {
         next();
       }
     })
     .catch((err) => {
       res.status(500).json({
-        error: "Error fetching spell."
+        error: "Error fetching spells."
       });
     });
 });
 
-function insertNewSpell(spell) {
+function insertNewSpell(spells) {
   return new Promise((resolve, reject) => {
     const spellValues = {
       id: null,
-      ownerID: spell.ownerID,
-      name: spell.name,
-	  cost: spell.cost,
-	  damage: spell.damage,
-	  school: spell.school
+      ownerID: spells.ownerID,
+      name: spells.name,
+	  cost: spells.cost,
+	  damage: spells.damage,
+	  school: spells.school
     };
     mysqlPool.query(
-      'INSERT INTO spell SET ?',
+      'INSERT INTO spells SET ?',
       spellValues,
       function (err, result) {
         if (err) {
@@ -158,35 +158,35 @@ function insertNewSpell(spell) {
 //	POST
 router.post('/', function (req, res, next) {
   if (validation.validateAgainstSchema(req.body, spellSchema)) {
-    let spell = validation.extractValidFields(req.body, spellSchema);
-	//check if spell already exists ? TODO
-    insertNewSpell(spell)
+    let spells = validation.extractValidFields(req.body, spellSchema);
+	//check if spells already exists ? TODO
+    insertNewSpell(spells)
 	 .then((id) => {
 		res.status(201).json({
 		id: id,
 		links: {
-			spell: `/spells/${id}`
+			spells: `/spells/${id}`
 		}
 		});
 	 })
 	 .catch((err) => {
 		 console.error(err);
 		 res.status(500).json({
-			error: "Error inserting spell." 
+			error: "Error inserting spells." 
 		 });
 	 });
     
   } else {
     res.status(400).json({
-      error: "Request body is not a valid spell object"
+      error: "Request body is not a valid spells object"
     });
   }
 });
 
-function replaceSpellByID(spellID, spell, mysqlPool) {
+function replaceSpellByID(spellID, spells, mysqlPool) {
   return new Promise((resolve, reject) => {
-    spell = validation.extractValidFields(spell, spellSchema);
-    mysqlPool.query('UPDATE spell SET ? WHERE id = ?', [ spell, spellID ], function (err, result) {
+    spells = validation.extractValidFields(spells, spellSchema);
+    mysqlPool.query('UPDATE spells SET ? WHERE id = ?', [ spells, spellID ], function (err, result) {
       if (err) {
         reject(err);
       } else {
@@ -211,7 +211,7 @@ router.put('/:spellID', requireAuthentication, function (req, res, next) {
 			if (updateSuccessful) {
 			  res.status(200).json({
 				links: {
-				  spell: `/spells/${spellID}`
+				  spells: `/spells/${spellID}`
 				}
 			  });
 			} else {
@@ -221,20 +221,20 @@ router.put('/:spellID', requireAuthentication, function (req, res, next) {
 		  .catch((err) => {
 			console.log(err);
 			res.status(500).json({
-			  error: "Unable to update specified spell.  Please try again later."
+			  error: "Unable to update specified spells.  Please try again later."
 			});
 		  });
 		}
   } else {
     res.status(400).json({
-      error: "Request body is not a valid spell object"
+      error: "Request body is not a valid spells object"
     });
   }
 });
 
 function deleteSpellByID(spellID, mysqlPool) {
   return new Promise((resolve, reject) => {
-    mysqlPool.query('DELETE FROM spell WHERE id = ?', [ spellID ], function (err, result) {
+    mysqlPool.query('DELETE FROM spells WHERE id = ?', [ spellID ], function (err, result) {
       if (err) {
         reject(err);
       } else {
@@ -264,7 +264,7 @@ router.delete('/:spellID', requireAuthentication, function (req, res, next) {
 		})
 		.catch((err) => {
 		  res.status(500).json({
-			error: "Unable to delete spell.  Please try again later."
+			error: "Unable to delete spells.  Please try again later."
 		  });
 		});
 	  }
